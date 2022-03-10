ff2d9eb368ff2c1da7feba10e96d383a9c399332
Kaz <takaik@yahoo.com>
2022-03-10 10:25:21 -0500

    add bonus solution

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'mast
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+
+  gem 'ffaker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# 3.times do
+#   actor = Actor.new({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, known_for: Faker::Artist.name})
+#   actor.save
+# end
+
 3.times do
-  actor = Actor.new({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, known_for: Faker::Artist.name})
+  actor = Actor.new({first_name: FFaker::NameJA.first_name, last_name: FFaker::NameJA.last_name, known_for: FFaker::Name.name_with_prefix})
   actor.save
 end
