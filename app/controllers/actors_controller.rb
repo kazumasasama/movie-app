7bc61dc00267f4992b762a6f639424ece89d57f9
Kaz <takaik@yahoo.com>
2022-03-21 20:17:18 -0400

    add association Actors <=> Movie

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -16,7 +16,8 @@ class ActorsController < ApplicationController
       last_name: params[:last_name],
       known_for: params[:known_for],
       gender: params[:gender],
-      age: params[:age]
+      age: params[:age],
+      movie_id: params[:movie_id]
     )
     if actor.save
       render json: actor.as_json
@@ -32,6 +33,7 @@ class ActorsController < ApplicationController
     actor.known_for = params[:known_for]
     actor.gender = params[:gender]
     actor.age = params[:age]
+    actor.movie_id = params[:movie_id]
     if actor.save
       render json: {message: "ID: #{actor.id} has been updated!"}
     else
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ApplicationRecord
+
+  has_many :actors #returns array of many actors
+
   validates :title, presence: true
   validates :year, numericality: { only_integer: true, greater_than: 1800}
   validates :plot, length: { maximum: 6000 }
   validates :english, presence: true
+  
 end
